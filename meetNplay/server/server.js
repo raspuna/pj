597bcdc04cd25c4b108e976fcbf04ab932badd72
raspuna <raspuna@gmail.com>
2022-06-10 15:15:33 -0700

    move pool into the mysql.config

--- meetNplay/server/server.js
+++ meetNplay/server/server.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 
 const express = require("express");
 const cors = require("cors");
-const pool = require("./config/mysql");
 const PORT = 8000;
 
 const app = express();
@@ -12,10 +11,6 @@ app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-  req.pool = pool;
-  next();
-});
 require("./routes/user.routes")(app);
 require("./routes/friend.routes")(app);
 
