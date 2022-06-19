2df639379f0f0d64554f26ad380e214302384495
raspuna <raspuna@gmail.com>
2022-06-18 22:46:54 -0700

    make easier to click  a playdate in the list

--- meetNplay/client/src/components/playdate/PlaydateList.js
+++ meetNplay/client/src/components/playdate/PlaydateList.js
@@ -28,30 +28,30 @@ function PlaydateList(props) {
       <p>upcoming({playdates.length})</p>
       {playdates.map((p) => {
         return (
-          <Card
-            bg={
-              new Date(p.start_time) < Date.now()
-                ? "secondary"
-                : CONST.rsvpColor(p.rsvp_status)
-            }
-            key={p.id}
-            className="mb-3"
-          >
-            {!isHost && (
-              <Card.Header>{CONST.rsvpTextOnly(p.rsvp_status)}</Card.Header>
-            )}
-            <Card.Body>
-              <Card.Title>
-                <Link to={`/playdate/${p.id}`}> {p.title}</Link>
-              </Card.Title>
-              {!isHost && <Card.Subtitle>by {p.name}</Card.Subtitle>}
-              <Card.Text>
-                {new Date(p.start_time).toLocaleDateString()}{" "}
-                {new Date(p.start_time).toLocaleTimeString()} ~
-                {new Date(p.end_time).toLocaleTimeString()}
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <Link className="text-decoration-none" to={`/playdate/${p.id}`}>
+            <Card
+              bg={
+                new Date(p.start_time) < Date.now()
+                  ? "secondary"
+                  : CONST.rsvpColor(p.rsvp_status)
+              }
+              key={p.id}
+              className="mb-3"
+            >
+              {!isHost && (
+                <Card.Header>{CONST.rsvpTextOnly(p.rsvp_status)}</Card.Header>
+              )}
+              <Card.Body>
+                <Card.Title>{p.title}</Card.Title>
+                {!isHost && <Card.Subtitle>by {p.name}</Card.Subtitle>}
+                <Card.Text>
+                  {new Date(p.start_time).toLocaleDateString()}{" "}
+                  {new Date(p.start_time).toLocaleTimeString()} ~
+                  {new Date(p.end_time).toLocaleTimeString()}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
         );
       })}
     </div>
